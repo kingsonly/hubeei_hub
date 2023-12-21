e1c340838c10389d1c223b4c935d78c806591471
kingsley achumie <kingsonly13c@gmail.com>
2023-12-21 13:19:28 +0100

    wip

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,88 +1,104 @@
-import React, { useState, useEffect, } from 'react';
-import Slide from './SlidePage';
-import SideIcons from './Sidebar';
-import Card from './Card';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import AppModal from './AppModal';
-import Contents from './contents';
-import axios from 'axios';
-import RankIcon from './RankIcon';
+import React, { useState, useEffect } from "react";
+import Slide from "./SlidePage";
+import SideIcons from "./Sidebar";
+import Card from "./Card";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import AppModal from "./AppModal";
+import Contents from "./contents";
+import axios from "axios";
+import RankIcon from "./RankIcon";
 
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 5
+    items: 5,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 4
+    items: 4,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 3
+    items: 3,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
+    items: 1,
+  },
 };
 
 function Main({ Rank, height, width }) {
   const [open, setOpen] = useState(false);
+  const [searchIcon, setSearchIcon] = useState(false);
+  const [searchLoaderStatus, setSearchLoaderStatus] = useState(false);
   const [selectedContent, setSelectedContent] = useState();
-  const [category, setCategory] = useState([])
+  const [category, setCategory] = useState([]);
   const [work, setWork] = useState(0);
   const [image, setImage] = useState(null);
   const [topContent, setTopContent] = useState([]);
 
   useEffect(() => {
-    createNewUser()
+    createNewUser();
     fetchAPI();
     fetchImage();
     fetchTopContent();
   }, []);
 
+  const setSearchIconUpdate = () => {
+    setSearchIcon(true);
+  };
+  const setSearchIconClose = () => {
+    setSearchIcon(false);
+  };
   const createNewUser = () => {
-    const user = localStorage.getItem('user');
+    const user = localStorage.getItem("user");
     if (!user) {
-      localStorage.setItem('user', UniqueId());
+      localStorage.setItem("user", UniqueId());
     }
-  }
+  };
   const fetchTopContent = async () => {
     try {
-      const response = await axios.get('https://api.hubeei.skillzserver.com/api/content/get-top-ten-views/4')
+      const response = await axios.get(
+        "https://api.hubeei.skillzserver.com/api/content/get-top-ten-views/4"
+      );
       if (response.data.status == "success") {
         setTopContent(response.data.data);
-        console.log('Top Content Data:', response.data);
+        console.log("Top Content Data:", response.data);
       }
-
     } catch (error) {
-      console.error('Error fetching top content:', error);
+      console.error("Error fetching top content:", error);
     }
   };
 
   const handleSearch = async (searchItem) => {
-
+    setSearchLoaderStatus(true);
     try {
-      const response = await fetch('https://api.hubeei.skillzserver.com/api/content/search/4', {
-        method: 'POST',
-        headers: {
-        },
-        body: JSON.stringify({ query: searchItem }),
-      });
+      const response = await fetch(
+        "https://api.hubeei.skillzserver.com/api/content/search/4",
+        {
+          method: "POST",
+          headers: {},
+          body: JSON.stringify({ query: searchItem }),
+        }
+      );
 
       const data = await response.json();
 
       if (data.status == "success") {
-
         setCategory(data.data);
+        setSearchLoaderStatus(false);
+        setSearchIcon(false);
       } else {
         console.error(`API request failed: ${response.statusText}`);
+        setSearchLoaderStatus(false);
+        setSearchIcon(false);
       }
     } catch (error) {
       console.error(`Error during API request:, ${error}`);
+      setSearchLoaderStatus(false);
+      setSearchIcon(false);
     }
   };
 
@@ -90,42 +106,41 @@ function Main({ Rank, height, width }) {
     setOpen(false);
   };
   const handleOpen = (items) => {
-    let works = work
-    setWork(works + 1)
-    setSelectedContent(items)
+    let works = work;
+    setWork(works + 1);
+    setSelectedContent(items);
     setOpen(true);
   };
 
-
-
   const fetchAPI = async () => {
-    const user = localStorage.getItem('user');
+    const user = localStorage.getItem("user");
     const headers = {
-      'user': user,
+      user: user,
     };
     await axios
-      .get("https://api.hubeei.skillzserver.com/api/category-content/4", { headers })
+      .get("https://api.hubeei.skillzserver.com/api/category-content/4", {
+        headers,
+      })
       .then((response) => {
-        let data = response.data
+        let data = response.data;
         if (data.status == "success") {
           setCategory(response.data.data);
         }
-
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   };
 
-
   const fetchImage = async () => {
     await axios
-      .get("https://api.hubeei.skillzserver.com/api/content/get-spotlight-content/4")
+      .get(
+        "https://api.hubeei.skillzserver.com/api/content/get-spotlight-content/4"
+      )
       .then((response) => {
-        let data = response.data
+        let data = response.data;
         if (data.status == "success") {
           setImage(response.data.data);
-
         }
       })
       .catch((error) => {
@@ -134,103 +149,121 @@ function Main({ Rank, height, width }) {
   };
 
   const UniqueId = () => {
-    return 'guest_' + Date.now();
-  }
-
+    return "guest_" + Date.now();
+  };
 
   return (
-    <div className='bg-black border-2 border-yellow-500 h-[100%]'>
+    <div className="bg-black border-2 border-yellow-500 h-[100%]">
       <AppModal open={open} handleClose={handleClose}>
-        {selectedContent != null ?
+        {selectedContent != null ? (
           <div className="fixed inset-0 flex flex-col items-center justify-center   space-y-[20px] h-[100%] w-[100%] mt-4">
-
             <div className=" p-8 rounded-lg shadow-lg  w-[80%]">
-              <h2 className='text-center'>{selectedContent ? selectedContent.name : 'Loading...'}</h2>
+              <h2 className="text-center">
+                {selectedContent ? selectedContent.name : "Loading..."}
+              </h2>
             </div>
 
-            <div className=" p-8 rounded-lg shadow-lg w-[100%] h-[100%]" style={{ overflowY: 'auto' }}>
-              <div className='text-center w-[70%] h-[100%]  mr-[15] border-2 border-rose-500'>
+            <div
+              className=" p-8 rounded-lg shadow-lg w-[100%] h-[100%]"
+              style={{ overflowY: "auto" }}
+            >
+              <div className="text-center w-[70%] h-[100%]  mr-[15] border-2 border-rose-500">
                 <Contents data={selectedContent} />
               </div>
 
               <div className=" p-8 rounded-lg shadow-lg w-[70%] border-2 border-yellow-500 ">
-                <div className='text-yellow-400  border-2 border-yellow-500 flex flex-col items-start flex flex justify-between'>
+                <div className="text-yellow-400  border-2 border-yellow-500 flex flex-col items-start flex flex justify-between">
                   <h1>Description</h1>
-                  <h3 className='text-white text-left'>{selectedContent.content_description !== null ? selectedContent.content_description : 'No Description'}</h3>
-                  <div className=''>
+                  <h3 className="text-white text-left">
+                    {selectedContent.content_description !== null
+                      ? selectedContent.content_description
+                      : "No Description"}
+                  </h3>
+                  <div className="">
                     <h1>hjfjj </h1>
                   </div>
                 </div>
-
               </div>
 
               <div className=" p-8 rounded-lg shadow-lg w-[80%] ">
-                <h2 className='text-center'>Engagement</h2>
+                <h2 className="text-center">Engagement</h2>
               </div>
               <div className=" p-8 rounded-lg shadow-lg w-[80%] h-[40%]">
-                <h2 className='text-center'>Things and Things</h2>
+                <h2 className="text-center">Things and Things</h2>
               </div>
-
             </div>
           </div>
-          : null
-
-        }
-
+        ) : null}
       </AppModal>
-      <div className='  '>
+      <div className="  ">
         <Slide handleOpen={handleOpen} data={image} />
       </div>
       <div className="fixed left-0 top-1/2 transform -translate-y-1/2 rounded z-20 ">
-        <SideIcons handleSearch={handleSearch} />
+        <SideIcons
+          handleSearch={handleSearch}
+          searchIcon={searchIcon}
+          setSearchIcon={setSearchIconUpdate}
+          setSearchIconClose={setSearchIconClose}
+          loaderStatus={searchLoaderStatus}
+        />
       </div>
-      <div className=' relative z-10 w-[100%]' >
-        <div className=''>
-        
-          <div className='ml-10'>
-            <div className=''>
-           
-              <Carousel responsive={responsive} infinite={true} keyBoardControl={true}>
-                {topContent.map((value, i,f) => <div>
-                  {console.log("Hi this na so we su",i)}
-                   <Card type={true} Rank={i+1} handleOpen={() => handleOpen(value)} content={value.name} id={value.id} imageUrl={`https://api.hubeei.skillzserver.com/public${value.thumbnail}`} />
-                   </div>
-                )}
-
+      <div className=" relative z-10 w-[100%]">
+        <div className="">
+          <div className="ml-10">
+            <div className="">
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                keyBoardControl={true}
+              >
+                {topContent.map((value, i, f) => (
+                  <div>
+                    {console.log("Hi this na so we su", i)}
+                    <Card
+                      type={true}
+                      Rank={i + 1}
+                      handleOpen={() => handleOpen(value)}
+                      content={value.name}
+                      id={value.id}
+                      imageUrl={`https://api.hubeei.skillzserver.com/public${value.thumbnail}`}
+                    />
+                  </div>
+                ))}
               </Carousel>
-              
             </div>
           </div>
         </div>
-        <div className=' w-[86%]' style={{ margin: "-20px auto" }}>
-          {category.length > 0 ? category.map((categoryItems) => {
-            return <div>
-              <h1 className='text-white'>{categoryItems.name} </h1>
-              <Carousel responsive={responsive} infinite={true} keyBoardControl={true}>
-
-                {categoryItems.content.map((items, i) => {
-
-                  return <Card handleOpen={() => handleOpen(items)} title={items.name} id={items.id} imageUrl={`https://api.hubeei.skillzserver.com/public${items.thumbnail}`} liked={items.like} />
-
-                })
-                }
-              </Carousel>
-            </div>
-          }) : console.log('khkh', category)}
-
+        <div className=" w-[86%]" style={{ margin: "-20px auto" }}>
+          {category.length > 0
+            ? category.map((categoryItems) => {
+                return (
+                  <div>
+                    <h1 className="text-white">{categoryItems.name} </h1>
+                    <Carousel
+                      responsive={responsive}
+                      infinite={true}
+                      keyBoardControl={true}
+                    >
+                      {categoryItems.content.map((items, i) => {
+                        return (
+                          <Card
+                            handleOpen={() => handleOpen(items)}
+                            title={items.name}
+                            id={items.id}
+                            imageUrl={`https://api.hubeei.skillzserver.com/public${items.thumbnail}`}
+                            liked={items.like}
+                          />
+                        );
+                      })}
+                    </Carousel>
+                  </div>
+                );
+              })
+            : console.log("khkh", category)}
         </div>
       </div>
-
-
     </div>
-  )
+  );
 }
 
-export default Main
-
-
-
-
-
-
-
+export default Main;
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,27 +1,40 @@
-import React, { useState } from 'react';
-import HomeIcon from '@mui/icons-material/Home';
-import SearchIcon from '@mui/icons-material/Search';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ExitToAppRoundedIcon from '@mui/icons-material/ExitToAppRounded';
-import Modal from '@mui/material/Modal';
-import { Box, Input, IconButton, } from '@mui/material';
+import React, { useState } from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ExitToAppRoundedIcon from "@mui/icons-material/ExitToAppRounded";
+import Modal from "@mui/material/Modal";
+import { Box, Input, IconButton, Button } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
 
+function SideIcons({
+  handleSearch,
+  searchIcon,
+  setSearchIcon,
+  setSearchIconClose,
+  loaderStatus,
+}) {
+  const [searchItem, setSearchItem] = useState("");
 
-function SideIcons({handleSearch}) {
-  const [searchIcon, setSearchIcon] = useState(false);
-  const [searchItem, setSearchItem] = useState('');
-
-  const handleSearchIcon = () => {
-    setSearchIcon(!searchIcon);
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 800,
+    bgcolor: "black",
+    p: 4,
+    height: 400,
+    overflow: "auto",
   };
 
   const onHandleSearch = async () => {
-    handleSearch(searchItem)
+    handleSearch(searchItem);
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       onHandleSearch();
     }
   };
@@ -29,44 +42,85 @@ function SideIcons({handleSearch}) {
   const iconSize = "w-8 h-[4rem]";
 
   return (
-    <div className="" >
-      <div className=' w-[50px] z-30 bg-[#000]/80 rounded'>
-        <div className={iconSize}><HomeIcon className='text-white cursor-pointer ml-2 mt-3' /></div>
-        <div className={iconSize}><SearchIcon
-          className={`text-white cursor-pointer ml-2 mt-2 ${searchIcon ? 'active' : ''}`}
-          onClick={handleSearchIcon}
-        /></div>
-        <div className={iconSize}><FavoriteBorderIcon className='text-white cursor-pointer ml-2 mt-2' /></div>
-        <div className={iconSize}><AccountCircleIcon className='text-white cursor-pointer ml-2 mt-2' /></div>
-        <div className={iconSize}><ExitToAppRoundedIcon className='text-white cursor-pointer ml-2 mt-2' /></div>
+    <div className="">
+      <div className=" w-[50px] z-30 bg-[#000]/80 rounded">
+        <div className={iconSize}>
+          <HomeIcon className="text-white cursor-pointer ml-2 mt-3" />
+        </div>
+        <div className={iconSize}>
+          <SearchIcon
+            className={`text-white cursor-pointer ml-2 mt-2 ${
+              searchIcon ? "active" : ""
+            }`}
+            onClick={setSearchIcon}
+          />
+        </div>
+        <div className={iconSize}>
+          <FavoriteBorderIcon className="text-white cursor-pointer ml-2 mt-2" />
+        </div>
+        <div className={iconSize}>
+          <AccountCircleIcon className="text-white cursor-pointer ml-2 mt-2" />
+        </div>
+        <div className={iconSize}>
+          <ExitToAppRoundedIcon className="text-white cursor-pointer ml-2 mt-2" />
+        </div>
 
         <Modal
           open={searchIcon}
-          onClose={handleSearchIcon}
+          onClose={setSearchIconClose}
           aria-labelledby="search-modal"
           aria-describedby="search-modal-description"
-          className="m-[15%]"
+          style={{ backdropFilter: "blur(5px)" }}
         >
-          <Box className="search-bar p-4 mx-auto rounded-lg" style={{ width: '800px', height: '60px' }}>
-            <div className="flex items-center">
-              <Input
-                type="text"
-                placeholder="Search..."
-                fullWidth
-                autoFocus
-                value={searchItem}
-                onChange={(e) => setSearchItem(e.target.value)}
-                onKeyPress={handleKeyPress}
-                sx={{
-                  backgroundColor: '#f0f0f0',
-                  boxShadow: ' ',
-                  borderRadius: '20px 0 0 20px', // Rounded corners on the left
-                  border: '2px solid yellow',
-                  height: '100%',
-                  width: 'calc(70% - 40px)', // Adjusted width to leave space for the button
-                  marginRight: '0',
-                }}
-              />
+          <Box style={style}>
+            <div className="flex">
+              <div>
+                <Input
+                  type="text"
+                  placeholder="Search..."
+                  fullWidth
+                  autoFocus
+                  value={searchItem}
+                  onChange={(e) => setSearchItem(e.target.value)}
+                  onKeyPress={handleKeyPress}
+                  disableUnderline
+                  sx={{
+                    backgroundColor: "#f0f0f0",
+                    paddingLeft: 5,
+                    borderTopLeftRadius: "20px",
+                    borderBottomLeftRadius: "20px",
+                    height: 40,
+                    width: 500, // Adjusted width to leave space for the button
+                    marginRight: "0",
+                  }}
+                />
+              </div>
+              <div>
+                <Button
+                  onClick={onHandleSearch}
+                  className="bg-[red]"
+                  sx={{
+                    backgroundColor: "#000",
+                    height: 40,
+                    borderTopLeftRadius: "0",
+                    borderBottomLeftRadius: "0",
+                    borderTopRightRadius: "20px",
+                    borderBottomRightRadius: "20px",
+                    color: "#DCD427",
+                  }}
+                >
+                  {loaderStatus ? (
+                    <CircularProgress
+                      sx={{
+                        color: "#DCD427",
+                      }}
+                      size={20}
+                    />
+                  ) : (
+                    "Search"
+                  )}
+                </Button>
+              </div>
             </div>
           </Box>
         </Modal>
