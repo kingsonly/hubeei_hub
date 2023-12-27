76930755026698e1f6797a2a63bfc673b6236e43
kingsley achumie <kingsonly13c@gmail.com>
2023-12-27 20:13:51 +0100

    wip

--- src/HeaderHero.jsx
+++ src/HeaderHero.jsx
@@ -66,12 +66,7 @@ function HeaderHero(props) {
                           }
                     }
                   >
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    {data.content_description}
                   </span>
                 </div>
               </div>
