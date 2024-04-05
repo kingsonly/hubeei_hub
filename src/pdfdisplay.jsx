185eb16d0e3986f1a481d5fcec628d2de4db4a8a
kingsley achumie <kingsonly13c@gmail.com>
2024-04-05 20:06:00 +0100

    wip

--- src/pdfdisplay.jsx
+++ src/pdfdisplay.jsx
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
 import { Document, Page } from "react-pdf";
+import pdffil from "./Images/cv-templates.pdf";
 
 function Pdfdisplay({ data }) {
   const [numPages, setNumPages] = useState();
   const [pageNumber, setPageNumber] = useState(1);
-  function onDocumentLoadSuccess(numPages) {
-    setNumPages(numPages);
+  // function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+  //   setNumPages(numPages);
+  // }
+
+  function onDocumentLoadSuccess(data) {
+    if (data && typeof data.numPages === "number") {
+      setNumPages(data.numPages);
+    } else {
+      // Handle potential errors (optional)
+      console.error("Invalid data received in onDocumentLoadSuccess");
+    }
   }
   return (
     <div>
-      <Document file="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdfhttps://drive.google.com/file/d/0B1HXnM1lBuoqMzVhZjcwNTAtZWI5OS00ZDg3LWEyMzktNzZmYWY2Y2NhNWQx/view?hl=en&resourcekey=0-5DqnTtXPFvySMiWstuAYdA">
-        <Page pageNumber={pageNumber} />
+      {/* <Document file={pdffil} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={1} />
       </Document>
-      {/* <object
-        title="Embedded PDF"
+      <p>
+        Page {pageNumber} of {numPages}
+      </p> */}
+
+      <iframe
         type="application/pdf"
-        data={
-          "https://api.hubeei.skillzserver.com/public/images/application/1710084270.pdf"
-        } // Replace with the URL to your PDF file
+        src={`/pdfjs-4.0.379-dist/web/viewer.html?file=https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf`}
+        // src={
+        //   "https://api.hubeei.skillzserver.com/public/images/application/1710084270.pdf"
+        // } // Replace with the URL to your PDF file
         width="100%"
         height="600px"
         allowFullScreen
-      >
-        
-      </object> */}
+      />
     </div>
   );
 }
